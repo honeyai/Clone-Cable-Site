0ee888b0ef56e85a4ea264670207ccc5e258403e
Jessica <contact.jessicasanchez@gmail.com>
2020-08-05 20:54:44 -0400

    added Home, header, and button components

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CardHeader from '@material-ui/core';
+
+const Header = ({title, subheader}) => {
+  return (
+    <CardHeader
+      title={title}
+      subheader={subheader}
+    />
+  );
+};
+
+export default Header;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Router } from 'react-router';
+
+const Navbar = () => {
+  return (
+    <Router>
+      <div>
+        
+      </div>
+    </Router>
+  );
+};
+
+export default Navbar;
