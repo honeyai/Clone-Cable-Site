bdae406119d2c09d85c65e0d365946076d522ef6
Jessica <contact.jessicasanchez@gmail.com>
2020-08-13 16:40:22 -0400

    lipsum lorum

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import {Typography} from '@material-ui/core';
+import '../Styles/home.css';
 
 const Home = () => {
   return (
-    <div>
+    <div className="home__Container">
+      <Typography variant="h3" className="home__Tagline">Connect with Cable</Typography>
       
     </div>
   );
