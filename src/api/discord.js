1a07752f97089293ee90c31ee4ec108c3c282053
Bubbles <bennovandorst@gmail.com>
2020-08-15 12:36:20 +0200

    login redirect url (this is the right one)

--- src/api/discord.js
+++ src/api/discord.js
@@ -7,7 +7,7 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const redirect = encodeURIComponent('http://localhost:3000/api/discord/callback');
 
 router.get('/login', (req, res) => {
-  res.redirect(`https://discord.com/api/oauth2/authorize?client_id=731919501523943514&permissions=8&redirect_uri=http%3A%2F%2Fcable.bennocraft.net%2F&scope=bot`);
+  res.redirect(`https://discord.com/api/oauth2/authorize?client_id=731919501523943514&redirect_uri=http%3A%2F%2Fcable.bennocraft.net%2F&response_type=code&scope=guilds%20identify`);
 });
 
 module.exports = router;
